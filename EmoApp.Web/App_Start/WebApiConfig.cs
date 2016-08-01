776c33a85c9257fa072ff9dbd808012e50a0da6c
Yordi <Yordi@DESKTOP-6E7D4HH>
2016-08-01 17:39:29 -0500

    Add SupportedMediaTypes in WebAPIConfig

--- EmoApp.Web/App_Start/WebApiConfig.cs
+++ EmoApp.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace EmoApp.Web
@@ -20,6 +21,7 @@ namespace EmoApp.Web
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
             config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
         }
